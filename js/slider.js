f95052cb1eb8a1dac65a02b84147b025036967d2
klara-novakova <novakovaklaraaa@seznam.cz>
2024-02-20 11:03:55 +0100

    ugh

--- js/slider.js
+++ js/slider.js
@@ -17,5 +17,3 @@ function mouseMoved(e) {
     slideOverlay = e.currentTarget.querySelector(".slider-view__back")
     slideOverlay.style.clipPath = `polygon(0 0, ${positionX}% 0, ${positionX}% 100%, 0 100%)`;
 }
-
-
